a503778bc498b6053a9fb055124e19dff25b3c8b
ryan <ryanpossanrubi@gmail.com>
2026-03-19 17:37:25 +0000

    modulo 1

---
+++ Modulo1/src/main/java/estga/estudy/modulo1/Modulo1.java
@@ -0,0 +1,20 @@
+package estga.estudy.modulo1;
+
+public class Modulo1 {
+
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        float altura;
+        boolean estudante;
+        
+        nome = "Paanda";
+        idade = 20;
+        altura = 1.75f;
+        estudante = true;
+        
+        
+        
+        System.out.println("O meu nome é " + nome + " tenho " + idade + "anos" + " meco " + altura + " e sou estudante: " + estudante);
+    }
+}
