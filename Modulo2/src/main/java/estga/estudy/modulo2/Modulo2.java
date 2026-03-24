725e1e2d70c6339090cec9420ab8f75a8fd6884b
ryan <ryanpossanrubi@gmail.com>
2026-03-24 18:22:10 +0000

    modulo 2

--- Modulo2/src/main/java/estga/estudy/modulo2/Modulo2.java
+++ Modulo2/src/main/java/estga/estudy/modulo2/Modulo2.java
@@ -6,13 +6,16 @@ public class Modulo2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int idade = 18; //idade a imprimir
+        String opcao = ""; //se fizermos isso garantimo que a opao é nula antes de entrar em while(garante que por acaso nao seja 3.
         
+        
+        while(!opcao.equals("3")){ //assim repete enquanto a opcao nao for igual a 3
         System.out.println("Escolha uma opção:");
         System.out.println("1 - Dizer Olá");
         System.out.println("2 - Mostrar idade");
         System.out.println("3 - Sair");
 
-        String opcao = sc.nextLine(); // criei apos o break pois estava a ficar confuso
+        opcao = sc.nextLine(); // criei apos o break pois estava a ficar confuso
         
         switch (opcao) { //aqui ele usara a opção entei meio confuso pensei em criar uma string e ler do teclado e jogar pta la
         
@@ -31,7 +34,7 @@ public class Modulo2 {
                 System.out.println("Opção invalida");
             
         }
-        
+        }
         sc.close();
         
         
